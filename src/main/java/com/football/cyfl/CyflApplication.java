476ba9402aa2ab255956bb1763e1584b558f80bc
Nicoaz25 <nicoadza2@gmail.com>
2026-05-18 15:32:39 +0200

    Primer commit

---
+++ src/main/java/com/football/cyfl/CyflApplication.java
@@ -0,0 +1,13 @@
+package com.football.cyfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyflApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/cyfl/CyflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.cyfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
